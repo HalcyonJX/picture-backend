027f13809e224c9d99215bbe428d8d827b000627
HalcyonJX <502229027@qq.com>
2025-03-20 21:36:59 +0800

    批量抓取和创建图片

--- src/main/java/com/halcyon/picturebackend/service/PictureService.java
+++ src/main/java/com/halcyon/picturebackend/service/PictureService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.halcyon.picturebackend.model.dto.picture.PictureQueryRequest;
 import com.halcyon.picturebackend.model.dto.picture.PictureReviewRequest;
+import com.halcyon.picturebackend.model.dto.picture.PictureUploadByBatchRequest;
 import com.halcyon.picturebackend.model.dto.picture.PictureUploadRequest;
 import com.halcyon.picturebackend.model.entity.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
@@ -81,4 +82,17 @@ public interface PictureService extends IService<Picture> {
      * @param loginUser
      */
     void fillReviewParams(Picture picture, User loginUser);
+
+    /**
+     * 批量抓取和创建图片
+     *
+     * @param pictureUploadByBatchRequest
+     * @param loginUser
+     * @return 成功创建的图片数
+     */
+    Integer uploadPictureByBatch(
+            PictureUploadByBatchRequest pictureUploadByBatchRequest,
+            User loginUser
+    );
+
 }
